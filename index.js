07032b88a8f292efe2159ccedaf4973445458164
ravikadia12 <ravmit12@gmail.com>
2026-01-20 17:17:55 +0530

    change port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./src/app");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5656;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
